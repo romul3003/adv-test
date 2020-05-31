efe63b2531fe387b8b4039400a2f2a56e0815f87
Roman Zakrevskyi <romul3003@gmail.com>
2020-05-31 18:26:15 +0300

    Add base layout

---
+++ components/MainContent/MainContent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './MainContent.module.scss'
+
+const MainContent = () => {
+  return (
+    <main className={styles.main}>Main</main>
+  )
+}
+
+export default MainContent
