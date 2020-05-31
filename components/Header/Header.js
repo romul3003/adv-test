bd989493d68559d0f7556f9148d06e6bee6a8ca3
Roman Zakrevskyi <romul3003@gmail.com>
2020-05-31 19:41:58 +0300

    Add header logo

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,7 +3,14 @@ import styles from './Header.module.scss'
 
 const Header = () => {
   return (
-    <header className={styles.header}>Header</header>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="/images/logo.svg" alt="logo"/>
+        <div className={styles.sign}>beta</div>
+      </div>
+
+
+    </header>
   )
 }
 
